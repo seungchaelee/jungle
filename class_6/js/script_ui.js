900880f331e967841efb41de0bdb1b256d93bc29
seungchaelee <id6639@naver.com>
2021-05-18 20:57:25 +0900

    5/18

--- class_6/js/script_ui.js
+++ class_6/js/script_ui.js
@@ -10,6 +10,18 @@ $(function(){
     $(".btn_setting").click(function(){
         reserveFn();
     });
+
+    //자리선택 완료 click 이벤트
+    $(".btn_submit").click(function(){
+        $(".section.reservation").hide();
+        $(".section.complete").show();
+    });
+
+    //완료후 리셋 click이벤트
+    $(".btn_reset").click(function(){
+        $(".section.complete").hide();
+        $(".box_intro").show();
+    });
 })
 
 //인트로 함수
@@ -22,6 +34,12 @@ function introFn(){
 function reserveFn(){
     $(".section.reservation").show();
     $(".box_intro").hide();
+
+    //자리선택 초기화 (li를 삭제처리)
+    $(".section.reservation > ol").empty();
+    //하단 선택정보 초기화
+    $(".txt_info_number").text("");
+    $(".txt_info_total").text(0);
 
     //json데이터 로드하는 ajax
     $.ajax({
@@ -43,17 +61,28 @@ function reserveFn(){
             $(".section.reservation > ol .unit > button").click(function(){
                 //select클래스 토글기능(있으면 빼주고 없으면 넣어주는 기능)
                 $(this).toggleClass("select");
-
+                
+                nameArr = []; //배열 초기화
+                totalPrice = 0; //총합계 초기화
                 //셋팅된 11개의 버튼을 탐색(each문을 통해)
                 $(".section.reservation > ol .unit > button").each(function(index){
                     //해당 순번의 버튼아 select라는 클래스가 있을 경우 true발생(hasClass를 통해)
                     if($(this).hasClass("select")){
                         //select를 갖고있는 버튼 순서에서 작동
-                        console.log(index);
-                        loadData[index]
-                        
+                        nameArr.push(loadData[index].name) //배열에 선택된 자리명을 추가해줌
+                        //totalPrice = totalPrice + loadData[index].price;
+                        totalPrice += Number(loadData[index].price); //총합계변수에 선탣된 자리가격을 더함
                     }
-                });
+                })
+                //console.log(totalPrice)
+
+                //자리 선택시 하단 정보를 업데이트
+                $(".txt_info_number").text(nameArr);
+                $(".txt_info_total").text(totalPrice);
+
+                //자리 선택시 왼료 페이지의 정보를 업데이트
+                $(".section.complete .txt_number").text(nameArr);
+                $(".section.complete .txt_price > strong").text(totalPrice);
             });
 
         }
