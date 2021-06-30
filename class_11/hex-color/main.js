6e0d8ada0a99ee88993924b8e03803b4d8ee69a6
seungchaelee <id6639@naver.com>
2021-07-01 02:36:28 +0900

    7/1

---
+++ class_11/hex-color/main.js
@@ -0,0 +1,21 @@
+const button = document.querySelector('#btn');
+
+// Add Event Listener
+
+button.addEventListener('click', () => {
+    const hexNumbers = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+    const value = document.querySelector('#hex-value');
+    const body = document.querySelector('body');
+    var hex = '#';
+
+    // Code Generate Loop Through
+    for(var i = 0; i < 6; i++) {
+        const index = Math.floor(Math.random() * hexNumbers.length);
+        hex += hexNumbers[index];
+    }
+
+    //Show On Screen
+    value.textContent = hex;
+    body.style.background = hex;
+
+});
